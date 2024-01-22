647536f249d5a64071653a303e43e4c549e8ae21
Carlos Roberto <crpu4a@gmail.com>
2024-01-22 18:05:08 -0300

    feat: add format seconds to string method

--- domain/FormatedFocus.ts
+++ domain/FormatedFocus.ts
@@ -14,8 +14,30 @@ export class FormatedFocus {
     ) {
         this.Id = id;
         this.Title = title;
-        this.RegisterDate = registerDate;
-        this.Duration = `${durationInSeconds}`;
+        this.RegisterDate = new Date(registerDate).toLocaleString("pt-BR");
+        this.Duration = this._secondsToString(durationInSeconds);
         this.CategoryName = categoryName;
+    }
+
+    private _secondsToString(secondsTotal: number) {
+        let timeString = "";
+        let secondsInitial = secondsTotal;
+
+        let hours = Math.floor(secondsInitial / 3600);
+        if (hours) {
+            timeString += `${hours}h `;
+            secondsInitial -= hours * 3600;
+        }
+
+        let minutes = Math.floor(secondsInitial / 60);
+        if (minutes) {
+            timeString += `${minutes}m `;
+            secondsInitial -= minutes * 60;
+        }
+
+        let seconds = secondsInitial;
+        if (seconds) timeString += `${seconds}s`;
+
+        return timeString;
     }
 }
