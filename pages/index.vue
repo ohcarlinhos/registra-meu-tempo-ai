1de7e497f5b17d89901f7bb08183460f24e92823
Carlos Roberto <crpu4a@gmail.com>
2024-01-22 10:48:38 -0300

    feat: create index page

---
+++ pages/index.vue
@@ -0,0 +1,12 @@
+<script lang="ts" setup></script>
+
+<template>
+    <UContainer
+        :ui="{
+            base: 'flex flex flex-row items-center',
+            constrained: 'min-h-svh max-w-xl',
+        }"
+    >
+        <GeneralHeader />
+    </UContainer>
+</template>
