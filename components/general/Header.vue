b9fcebac5a659a7615f540c3ab5ba1e9c4327d97
Carlos Roberto <crpu4a@gmail.com>
2024-01-22 17:33:36 -0300

    feat: change Header to have conditionals

--- components/general/Header.vue
+++ components/general/Header.vue
@@ -1,22 +1,50 @@
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import { useAuthStore } from "~/store/AuthStore";
+
+const { getUserToken, clearUserToken } = useAuthStore();
+const userToken = getUserToken;
+
+const exit = () => {
+    clearUserToken();
+    useRouter().push("login");
+};
+
+const activeClass = "text-primary font-bold";
+</script>
 
 <template>
     <header class="pb-20">
         <section>
-            <h1 class="font-bold text-6xl text-teal-500">Pomodoro v2</h1>
+            <h1 class="font-bold text-6xl text-primary">Pomodoro v2</h1>
             <p class="pt-4">
                 Versão 2 do
-                <span class="font-bold text-teal-500">Pomodoro</span> com
+                <span class="font-bold text-primary">Pomodoro</span> com
                 integração "total" com API.
             </p>
         </section>
         <nav class="pt-6 flex gap-4">
-            <ULink to="/" active-class="text-teal-500">Página Inicial</ULink>
-            <ULink to="/dashboard" active-class="text-teal-500">Painel</ULink>
-            <ULink to="/login" active-class="text-teal-500">Acesso</ULink>
-            <ULink to="/register" active-class="text-teal-500">
+            <ULink to="/" :active-class="activeClass">Página Inicial</ULink>
+
+            <ULink v-if="userToken" to="/dashboard" :active-class="activeClass">
+                Painel
+            </ULink>
+
+            <ULink v-if="!userToken" to="/login" :active-class="activeClass">
+                Acesso
+            </ULink>
+
+            <ULink v-if="!userToken" to="/register" :active-class="activeClass">
                 Registrar
             </ULink>
+
+            <UButton
+                v-if="userToken"
+                variant="link"
+                :padded="false"
+                size="xl"
+                label="Sair"
+                @click="exit"
+            />
         </nav>
     </header>
 </template>
