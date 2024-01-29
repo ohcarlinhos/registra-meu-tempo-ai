06ad08c43f2940f2035c59d50b60a9df51600a55
Carlos Roberto <crpu4a@gmail.com>
2024-01-29 09:49:04 -0300

    feat: split auth middlewares on auth and guest

--- middleware/auth.ts
+++ middleware/auth.ts
@@ -1,11 +1,4 @@
 export default defineNuxtRouteMiddleware((to) => {
-    const name = to.name?.toString() || "";
     const token = useAuthStore().getUserToken;
-
-    if (name && ["login", "register"].includes(name)) {
-        if (token) return navigateTo("/dashboard");
-        return;
-    }
-
     if (!token) return navigateTo("/");
 });
