9ab173c926ce7860ce47fbec75a9744ce71b3809
Carlos Roberto <crpu4a@gmail.com>
2024-03-03 23:42:29 -0300

    style: formatação

--- pages/index.vue
+++ pages/index.vue
@@ -1,12 +1,10 @@
-<script lang="ts" setup></script>
-
 <template>
-    <UContainer
-        :ui="{
-            base: 'flex flex flex-row items-center',
-            constrained: 'min-h-svh max-w-xl',
-        }"
-    >
-        <GeneralHeader />
-    </UContainer>
+  <UContainer
+    :ui="{
+      base: 'flex flex-row items-center',
+      constrained: 'min-h-svh max-w-xl',
+    }"
+  >
+    <GeneralHeader />
+  </UContainer>
 </template>
