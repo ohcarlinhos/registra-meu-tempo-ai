efc339ac66ad9f246e1cb1fb42613155ad24fbe0
Carlos Roberto <crpu4a@gmail.com>
2024-03-03 23:43:52 -0300

    chore: adição de vue-datepicker

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,14 +1,18 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-    devtools: { enabled: true },
-    modules: ["@nuxt/ui", "@pinia/nuxt", "@pinia-plugin-persistedstate/nuxt"],
-    ui: { global: true },
-    imports: {
-        dirs: ["composables/**"],
+  devtools: { enabled: true },
+  ssr: false,
+  modules: ["@nuxt/ui", "@pinia/nuxt", "@pinia-plugin-persistedstate/nuxt"],
+  ui: { global: true },
+  imports: {
+    dirs: ["composables/**"],
+  },
+  build: {
+    transpile: ["@vuepic/vue-datepicker"],
+  },
+  runtimeConfig: {
+    public: {
+      apiBase: process.env.API_BASE,
     },
-    runtimeConfig: {
-       public: {
-        apiBase: process.env.API_BASE
-       } 
-    }
+  },
 });
