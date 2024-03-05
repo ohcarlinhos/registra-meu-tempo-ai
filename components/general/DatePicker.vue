cc18d3c7aa9b0c2d3ea6feb3b0569de387915684
Carlos Roberto <crpu4a@gmail.com>
2024-03-05 06:34:55 -0300

    feat: adicionar name ao DatePicker.vue

--- components/general/DatePicker.vue
+++ components/general/DatePicker.vue
@@ -4,67 +4,73 @@ import VDatePicker from "@vuepic/vue-datepicker";
 import { set } from "date-fns";
 
 const props = defineProps({
-  modelValue: {
-    type: [Date, String],
-    default: "",
-  },
+	modelValue: {
+		type: [Date, String],
+		default: ""
+	},
 
-  min: {
-    type: [Date, String],
-    default: "",
-  },
+	min: {
+		type: [Date, String],
+		default: ""
+	},
 
-  max: {
-    type: [Date, String],
-    default: "",
-  },
+	max: {
+		type: [Date, String],
+		default: ""
+	},
 
-  time: {
-    type: Boolean,
-    default: false,
-  },
+	time: {
+		type: Boolean,
+		default: false
+	},
 
-  disableTimePicker: {
-    type: Boolean,
-    default: false,
-  },
+	disableTimePicker: {
+		type: Boolean,
+		default: false
+	},
+
+	name: {
+		type: String,
+		default: ""
+	}
 });
 
 const emit = defineEmits(["update:model-value", "change"]);
 
 const date = computed({
-  get: () => props.modelValue,
+	get: () => props.modelValue,
 
-  set: (value) => {
-    if (props.disableTimePicker && value) {
-      value = set(new Date(value), {
-        hours: 0,
-        minutes: 0,
-        seconds: 0,
-        milliseconds: 0,
-      });
-    }
-    emit("update:model-value", value);
-    emit("change", value);
-  },
+	set: (value) => {
+		if (props.disableTimePicker && value) {
+			value = set(new Date(value), {
+				hours: 0,
+				minutes: 0,
+				seconds: 0,
+				milliseconds: 0
+			});
+		}
+		emit("update:model-value", value);
+		emit("change", value);
+	}
 });
 </script>
 
 <template>
-  <VDatePicker
-    v-model="date"
-    :is24="true"
-    :action-row="{ showNow: true }"
-    :min-date="min || ''"
-    :max-date="max || ''"
-    :time-picker="time"
-    :enable-time-picker="disableTimePicker === false"
-    :teleport="true"
-    :format="disableTimePicker ? 'dd/MM/yyyy' : 'dd/MM/yyyy HH:mm'"
-    locale="pt-BR"
-    select-text="Selecionar"
-    cancel-text="Fechar"
-    now-button-label="Agora"
-    dark
-  />
+	<VDatePicker
+		v-model="date"
+		:is24="true"
+		:action-row="{ showNow: true }"
+		:min-date="min || ''"
+		:max-date="max || ''"
+		:time-picker="time"
+		:enable-time-picker="disableTimePicker === false"
+		:teleport="true"
+		:format="disableTimePicker ? 'dd/MM/yyyy' : 'dd/MM/yyyy HH:mm'"
+		:name="name"
+		locale="pt-BR"
+		select-text="Selecionar"
+		cancel-text="Fechar"
+		now-button-label="Agora"
+		dark
+	/>
 </template>
