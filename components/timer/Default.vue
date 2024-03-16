3792fab923d7614db5ade7df4fb39fc8d73b4a63
Carlos Roberto <crpu4a@gmail.com>
2024-03-16 10:21:01 -0300

    feat: adição de componente de card ao timer default

--- components/timer/Default.vue
+++ components/timer/Default.vue
@@ -5,17 +5,24 @@ const store = useTimerStore();
 </script>
 
 <template>
-  <div class="flex flex-col gap-5 justify-center items-center">
-    <h3 class="text-6xl font-bold">{{ store.formated }}</h3>
+  <UCard>
+    <div class="flex flex-col gap-5 justify-center items-center">
+      <h3 class="text-6xl font-bold">{{ store.formated }}</h3>
 
-    <div class="flex gap-3">
-      <UButton
-        :label="store.currentPeriodoList.length ? 'Continuar' : 'Iniciar'"
-        :disabled="store.running"
-        @click="store.start"
-      />
-      <UButton label="Parar" @click="store.pause" :disabled="!store.running" />
-      <UButton label="Finalizar" @click="store.end" />
+      <div class="flex gap-3">
+        <UButton
+          :label="store.currentPeriodoList.length ? 'Continuar' : 'Iniciar'"
+          :disabled="store.running"
+          @click="store.start"
+        />
+
+        <UButton
+          label="Parar"
+          @click="store.pause"
+          :disabled="!store.running"
+        />
+        <UButton label="Finalizar" @click="store.end" />
+      </div>
     </div>
-  </div>
+  </UCard>
 </template>
