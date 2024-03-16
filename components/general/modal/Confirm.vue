16de03c27974af44f403a9159d045131562234d6
Carlos Roberto <crpu4a@gmail.com>
2024-03-16 13:18:30 -0300

    feat: criação do componente modal de confirmação

---
+++ components/general/modal/Confirm.vue
@@ -0,0 +1,47 @@
+<script lang="ts" setup>
+const props = defineProps<{
+  open: boolean;
+  text: string;
+}>();
+
+const emit = defineEmits(["update:open", "confirm", "cancel"]);
+
+const isOpen = computed({
+  get: () => {
+    return props.open;
+  },
+  set: (value: boolean) => {
+    emit("update:open", value);
+  },
+});
+</script>
+
+<template>
+  <UModal
+    v-model="isOpen"
+    prevent-close
+    :ui="{
+      width: 'sm:w-72',
+    }"
+  >
+    <UCard>
+      {{ text }}
+
+      <template #footer>
+        <div class="grid grid-cols-2 gap-5">
+          <UButton
+            color="blue"
+            block
+            label="Cancelar"
+            @click="emit('cancel')"
+          />
+
+          <UButton block label="Confirmar" @click="emit('confirm')" />
+        </div>
+        <!-- <Placeholder class="h-8" /> -->
+      </template>
+    </UCard>
+  </UModal>
+</template>
+
+<style></style>
