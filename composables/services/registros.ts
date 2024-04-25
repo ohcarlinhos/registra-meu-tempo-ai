cc4e23825cbbd5a3d2076043e69c5bf6085a8408
Carlos Júnior <crpu4a@gmail.com>
2024-04-25 18:21:15 -0300

    fix: correção das urls

--- composables/services/registros.ts
+++ composables/services/registros.ts
@@ -1,23 +1,17 @@
 export const getRegistros = async function () {
   return await CustomHttp<null, RegistroType[]>(
     // TODO: remover parâmetros mocados
-    "/registro-de-tempo?page=1&perPage=24",
+    "/registro?page=1&perPage=24",
     "get"
   );
 };
 
 export const getRegistroById = async function (id: number) {
-  return await CustomHttp<null, RegistroType>(
-    `/registro-de-tempo/${id}`,
-    "get"
-  );
+  return await CustomHttp<null, RegistroType>(`/registro/${id}`, "get");
 };
 
 export const deleteRegistro = async (id: number) => {
-  return await CustomHttp<null, RegistroType>(
-    `/registro-de-tempo/${id}`,
-    "delete"
-  );
+  return await CustomHttp<null, RegistroType>(`/registro/${id}`, "delete");
 };
 
 type Payload = {
@@ -29,9 +23,5 @@ type Payload = {
 export const postRegistro = async (payload: Payload) => {
   if (payload.categoriaId == null) delete payload.categoriaId;
 
-  return CustomHttp<Payload, RegistroType>(
-    "/registro-de-tempo",
-    "post",
-    payload
-  );
+  return CustomHttp<Payload, RegistroType>("/registro", "post", payload);
 };
