eb6058b1a2d79479bdf5bb9937f41cbb1fab5362
Carlos Júnior <crpu4a@gmail.com>
2024-05-16 21:48:26 -0300

    feat: create panel layout

---
+++ layouts/Panel.vue
@@ -0,0 +1,13 @@
+<script lang="ts" setup></script>
+
+<template>
+  <UContainer
+    :ui="{
+      padding: 'py-16',
+      constrained: 'min-h-svh',
+    }"
+  >
+    <GHeader small-title />
+    <slot></slot>
+  </UContainer>
+</template>
