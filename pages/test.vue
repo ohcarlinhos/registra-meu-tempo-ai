24d2324616b91ee0350238fd67c2b4390c93bedc
Carlos Roberto <crpu4a@gmail.com>
2024-08-03 00:34:06 -0300

    feat: add page test with notification request

---
+++ pages/test.vue
@@ -0,0 +1,22 @@
+<script lang="ts" setup>
+const askToAuthorizateNotification = () => {
+  Notification.requestPermission((permission) => {
+    if (permission === "granted") {
+      setTimeout(() => {
+        clickSound.play();
+        new Notification("Esse é apenas um teste, sabia?", {
+          body: "Corpo da notificação",
+        });
+      }, 3000);
+    }
+  });
+};
+</script>
+
+<template>
+  <div class="p-16 w-full flex justify-center">
+    <UButton @click="askToAuthorizateNotification" label="Emitir notificação" />
+  </div>
+</template>
+
+<style></style>
