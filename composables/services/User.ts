ab138c6abbca1d61a79dfe4c14e4a4673d21d174
Carlos Júnior <crpu4a@gmail.com>
2024-08-23 20:07:33 -0300

    feat: add UserMap type

--- composables/services/User.ts
+++ composables/services/User.ts
@@ -1,23 +1,11 @@
 export const postUser = async (payload: CreateUserDto) => {
-  return CustomHttp<CreateUserDto, unknown>("user", "post", payload, true);
-};
-
-type UpdateUserResponse = {
-  name?: string;
-  email?: string;
-  password?: string;
-  oldPassword?: string;
+  return CustomHttp<CreateUserDto, UserMap>("user", "post", payload, true);
 };
 
 export const updateUser = async (id: number, payload: UpdateUserDto) => {
-  return CustomHttp<UpdateUserDto, UpdateUserResponse>(
-    `user/${id}`,
-    "put",
-    payload,
-    true
-  );
+  return CustomHttp<UpdateUserDto, UserMap>(`user/${id}`, "put", payload, true);
 };
 
 export const getMyself = async (mounted = false) => {
-  return CustomHttp<null, UserType>("user/myself", "get", null, mounted);
+  return CustomHttp<null, UserMap>("user/myself", "get", null, mounted);
 };
