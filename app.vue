0d9febf25476d18a84755d9b04ba96bb37ddc0db
Carlos Júnior <crpu4a@gmail.com>
2024-08-29 22:48:28 -0300

    feat: add old token button and login modal to app.vue

--- app.vue
+++ app.vue
@@ -8,6 +8,13 @@ const colorMode = useColorMode();
 const toggleDark = () => {
   colorMode.preference = colorMode.value === "dark" ? "light" : "dark";
 };
+
+const authStore = useAuthStore();
+const cs = useConfigStore();
+
+const setOldToken = () => {
+  authStore.setOldToken();
+};
 </script>
 
 <template>
@@ -26,6 +33,15 @@ const toggleDark = () => {
       aria-label="Theme"
       @click="toggleDark"
     />
+
+    <section v-if="cs.hasWarTools" class="fixed top-2 right-2 z-50">
+      <UButton @click="setOldToken"> Set Old Token </UButton>
+    </section>
+
+    <UModal v-model="authStore._openModal" prevent-close>
+      <AuthFormLogin v-if="authStore._openModal" />
+    </UModal>
+
     <NuxtPage />
   </NuxtLayout>
 
