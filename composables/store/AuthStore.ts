9fe1d61f3e8d594cf0078e89416a033189a0a990
Carlos Júnior <crpu4a@gmail.com>
2024-08-29 22:47:25 -0300

    feat: update auth store

--- composables/store/AuthStore.ts
+++ composables/store/AuthStore.ts
@@ -2,14 +2,34 @@ export const useAuthStore = defineStore("AuthStore", {
   state: () => {
     return {
       _userToken: "",
+      _openModal: false,
+      _needRefreshBeforeLogin: false,
     };
   },
   actions: {
     setUserToken(token: string) {
       this._userToken = token;
     },
+
     clearUserToken() {
       this._userToken = "";
+    },
+
+    setOldToken() {
+      this._userToken = useConfigStore().oldUserToken;
+    },
+
+    openModal(refresh = false) {
+      if (refresh) this._needRefreshBeforeLogin = true;
+      this._openModal = true;
+    },
+
+    closeModal() {
+      this._openModal = false;
+      if (this._needRefreshBeforeLogin) {
+        this._needRefreshBeforeLogin = false;
+        location.reload();
+      }
     },
   },
   getters: {
@@ -17,6 +37,10 @@ export const useAuthStore = defineStore("AuthStore", {
 
     isAuth: (state) => {
       return state._userToken != "";
+    },
+
+    needRefresh: (state) => {
+      return state._needRefreshBeforeLogin;
     },
   },
   persist: true,
