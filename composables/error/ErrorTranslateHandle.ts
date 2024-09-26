cc708a2dc2b18dc18015ebb37c18840479debef6
Carlos Júnior <crpu4a@gmail.com>
2024-09-26 12:51:35 -0300

    feat: add error to ErrorTranslateList

--- composables/error/ErrorTranslateHandle.ts
+++ composables/error/ErrorTranslateHandle.ts
@@ -2,6 +2,8 @@ const ErrorTranslateList = [
   ["check_your_email_inbox", "checkYourEmailInbox"],
   ["wrong_email_or_password", "wrongEmailOrPassword"],
   ["user_not_found", "userNotFound"],
+  ["recovery_code_not_found", "invalidRecoveryCode"],
+  ["recovery_code_is_used_or_expired", "invalidRecoveryCode"],
 ];
 
 export const TranslateErrorHandle = (tag: string) => {
