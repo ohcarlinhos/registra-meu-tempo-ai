4109d965d048464b35bb810a20c573777a65d2b5
Carlos Roberto <crpu4a@gmail.com>
2024-12-24 16:19:47 -0300

    feat: adicionar persistência em store de usuário.

--- composables/store/user.ts
+++ composables/store/user.ts
@@ -1,54 +1,60 @@
 import { defineStore } from "pinia";
 
-export const useUserStore = defineStore("user-store", () => {
-  const mySelf = ref<UserMap>();
-  const isFetch = ref(false);
+export const useUserStore = defineStore(
+  "user-store",
+  () => {
+    const mySelf = ref<UserMap>();
+    const isFetch = ref(false);
 
-  const fetchMySelf = async (callback: (data: UserMap) => void = () => {}) => {
-    try {
-      isFetch.value = true;
-      const data = await getMySelf();
+    const fetchMySelf = async (
+      callback: (data: UserMap) => void = () => {}
+    ) => {
+      try {
+        isFetch.value = true;
+        const data = await getMySelf();
 
-      if (data) {
-        mySelf.value = data;
-        callback(data);
+        if (data) {
+          mySelf.value = data;
+          callback(data);
+        }
+      } catch (error) {
+        ErrorToast(error);
+      } finally {
+        isFetch.value = false;
       }
-    } catch (error) {
-      ErrorToast(error);
-    } finally {
-      isFetch.value = false;
-    }
-  };
+    };
 
-  const isVerified = computed(() => {
-    return Boolean(mySelf.value?.isVerified);
-  });
+    const isVerified = computed(() => {
+      return Boolean(mySelf.value?.isVerified);
+    });
 
-  const isActive = computed(() => {
-    return Boolean(mySelf.value?.isVerified);
-  });
+    const isActive = computed(() => {
+      return Boolean(mySelf.value?.isVerified);
+    });
 
-  const checkIfIsVerified = async () => {
-    await fetchMySelf();
+    const checkIfIsVerified = async () => {
+      await fetchMySelf();
 
-    if (mySelf.value?.isVerified == false) {
-      console.log("Não está verificado.");
-    }
+      if (mySelf.value?.isVerified == false) {
+        console.log("Não está verificado.");
+      }
 
-    return mySelf.value?.isVerified;
-  };
+      return mySelf.value?.isVerified;
+    };
 
-  const clearMySelf = () => {
-    mySelf.value = undefined;
-  };
+    const clearMySelf = () => {
+      mySelf.value = undefined;
+    };
 
-  return {
-    mySelf,
-    isFetch,
-    isActive,
-    isVerified,
-    fetchMySelf,
-    checkIfIsVerified,
-    clearMySelf,
-  };
-});
+    return {
+      mySelf,
+      isFetch,
+      isActive,
+      isVerified,
+      fetchMySelf,
+      checkIfIsVerified,
+      clearMySelf,
+    };
+  },
+  { persist: true }
+);
