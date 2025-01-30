aeaa24373abc006b22e3fbb8caf682ef624495ce
Carlos Júnior <crpu4a@gmail.com>
2025-01-30 16:19:33 -0300

    feat: adição de fallback para cronômetro

--- pages/index.vue
+++ pages/index.vue
@@ -7,6 +7,14 @@ useHead({ title: "Cronômetros" });
   <NuxtLayout name="center">
     <ClientOnly>
       <TimerDefault title="Cronômetros" />
+
+      <template #fallback>
+        <div class="flex gap-5 flex-col w-full items-center">
+          <USkeleton class="h-[50px] w-[90%]" />
+          <USkeleton class="h-[210px] w-full" />
+          <USkeleton class="h-[40px] w-[60%]" />
+        </div>
+      </template>
     </ClientOnly>
   </NuxtLayout>
 </template>
