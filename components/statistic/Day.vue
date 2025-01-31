8e7f81497a6a647c117593e053c63ebd93d5a69c
Carlos Júnior <crpu4a@gmail.com>
2025-01-31 16:25:56 -0300

    feat: separação de skeleton

--- components/statistic/Day.vue
+++ components/statistic/Day.vue
@@ -224,31 +224,7 @@ var maxDate = ref(new Date(Date.now()));
       </section>
     </section>
 
-    <section
-      v-if="hasFetch"
-      class="w-full grid grid-cols-4 items-start gap-4 md:gap-4"
-    >
-      <section class="col-span-full">
-        <USkeleton class="h-8 w-32" />
-      </section>
-
-      <USkeleton
-        class="col-span-2 row-span-2 h-full flex items-center justify-center"
-      />
-
-      <USkeleton class="h-44" />
-      <USkeleton class="h-44" />
-      <USkeleton class="h-44" />
-      <USkeleton class="h-44" />
-
-      <section class="col-span-full">
-        <USkeleton class="h-8 w-44 mt-8" />
-      </section>
-
-      <USkeleton class="h-44" />
-      <USkeleton class="h-44" />
-      <USkeleton class="h-44" />
-    </section>
+    <StatisticSkeleton v-if="hasFetch" />
 
     <section v-else class="w-full grid grid-cols-1 items-start gap-5">
       <section
---
+++ components/statistic/Skeleton.vue
@@ -0,0 +1,24 @@
+<template>
+  <section class="w-full grid grid-cols-4 items-start gap-4 md:gap-4">
+    <section class="col-span-full">
+      <USkeleton class="h-8 w-32" />
+    </section>
+
+    <USkeleton
+      class="col-span-2 row-span-2 h-full flex items-center justify-center"
+    />
+
+    <USkeleton class="h-44" />
+    <USkeleton class="h-44" />
+    <USkeleton class="h-44" />
+    <USkeleton class="h-44" />
+
+    <section class="col-span-full">
+      <USkeleton class="h-8 w-44 mt-8" />
+    </section>
+
+    <USkeleton class="h-44" />
+    <USkeleton class="h-44" />
+    <USkeleton class="h-44" />
+  </section>
+</template>
