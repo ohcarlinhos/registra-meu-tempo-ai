7a293a3cb7c1245e8ce620e4257ae4f45719ff44
Carlos Júnior <crpu4a@gmail.com>
2025-02-04 17:31:06 -0300

    feat: rascunho da versão 2 do painel

---
+++ components/layout/panel/Footer.vue
@@ -0,0 +1,107 @@
+<script lang="ts" setup>
+import {
+  BadgeCheck,
+  Bell,
+  ChevronsUpDown,
+  CreditCard,
+  LogOut,
+  Sparkles,
+} from "lucide-vue-next";
+
+const data = {
+  user: {
+    name: "Carlos Roberto",
+    email: "teste@email.com",
+    avatar: "/avatars/shadcn.jpg",
+  },
+};
+</script>
+
+<template>
+  <SidebarFooter>
+    <SidebarMenu>
+      <SidebarMenuItem>
+        <DropdownMenu>
+          <DropdownMenuTrigger as-child>
+            <SidebarMenuButton
+              size="lg"
+              class="data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground"
+            >
+              <Avatar class="h-8 w-8 rounded-lg">
+                <AvatarImage :src="data.user.avatar" :alt="data.user.name" />
+                <AvatarFallback class="rounded-lg"> CR </AvatarFallback>
+              </Avatar>
+              <div class="grid flex-1 text-left text-sm leading-tight">
+                <span class="truncate font-semibold">
+                  {{ data.user.name }}
+                </span>
+                <span class="truncate text-xs">{{ data.user.email }}</span>
+              </div>
+              <ChevronsUpDown class="ml-auto size-4" />
+            </SidebarMenuButton>
+          </DropdownMenuTrigger>
+
+          <DropdownMenuContent
+            class="w-[--radix-dropdown-menu-trigger-width] min-w-56 rounded-lg"
+            side="bottom"
+            align="end"
+            :side-offset="4"
+          >
+            <DropdownMenuLabel class="p-0 font-normal">
+              <div
+                class="flex items-center gap-2 px-1 py-1.5 text-left text-sm"
+              >
+                <Avatar class="h-8 w-8 rounded-lg">
+                  <AvatarImage :src="data.user.avatar" :alt="data.user.name" />
+                  <AvatarFallback class="rounded-lg"> CN </AvatarFallback>
+                </Avatar>
+                <div class="grid flex-1 text-left text-sm leading-tight">
+                  <span class="truncate font-semibold">
+                    {{ data.user.name }}
+                  </span>
+
+                  <span class="truncate text-xs">{{ data.user.email }}</span>
+                </div>
+              </div>
+            </DropdownMenuLabel>
+
+            <!-- <DropdownMenuSeparator />
+
+            <DropdownMenuGroup>
+              <DropdownMenuItem>
+                <Sparkles />
+                Upgrade to Pro
+              </DropdownMenuItem>
+            </DropdownMenuGroup> -->
+
+            <DropdownMenuSeparator />
+
+            <DropdownMenuGroup>
+              <DropdownMenuItem @select="$router.push({ name: 'user.me' })">
+                <BadgeCheck />
+                Minha Conta
+              </DropdownMenuItem>
+
+              <!-- <DropdownMenuItem>
+                <CreditCard />
+                Billing
+              </DropdownMenuItem>
+
+              <DropdownMenuItem>
+                <Bell />
+                Notifications
+              </DropdownMenuItem> -->
+            </DropdownMenuGroup>
+
+            <DropdownMenuSeparator />
+
+            <DropdownMenuItem>
+              <LogOut />
+              Sair
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      </SidebarMenuItem>
+    </SidebarMenu>
+  </SidebarFooter>
+</template>
