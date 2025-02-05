cf5102a5beb87737a6742d00bc9f429cb53bb82c
Carlos Júnior <crpu4a@gmail.com>
2025-02-05 14:24:12 -0300

    feat: criação de página de feedback

---
+++ pages/feedback.vue
@@ -0,0 +1,17 @@
+<script lang="ts" setup>
+definePageMeta({
+  middleware: ["auth"],
+  name: "feedback.page",
+  props: { breadcrumb: ["Feedback"] },
+});
+</script>
+
+<template>
+  <NuxtLayout name="panel-v2">
+    <div class="grid grid-cols-1 md:grid-cols-12 gap-16 md:gap-5 w-full">
+      <div class="w-full col-span-1 md:col-span-6 lg:col-span-4">
+        <FeedbackFormCreate />
+      </div>
+    </div>
+  </NuxtLayout>
+</template>
