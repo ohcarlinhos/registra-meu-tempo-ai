5826f6a9f79199f17710c891c4b3024084dc4574
Carlos Júnior <crpu4a@gmail.com>
2025-02-05 13:10:46 -0300

    feat: atualização card

--- components/time-record/card/About.vue
+++ components/time-record/card/About.vue
@@ -5,35 +5,37 @@ defineProps<{
 </script>
 
 <template>
-  <UCard>
-    <p class="text-lg pb-2">
-      <span v-if="timeRecord.description" class="font-normal">
-        {{ timeRecord.description }}
-      </span>
-      <span v-else class="font-normal">
-        {{ _$t("emptyRecordDescription") }}
-      </span>
-    </p>
+  <Card>
+    <CardContent>
+      <p class="text-lg mt-6 pb-2">
+        <span v-if="timeRecord.description" class="font-normal">
+          {{ timeRecord.description }}
+        </span>
+        <span v-else class="font-normal">
+          {{ _$t("emptyRecordDescription") }}
+        </span>
+      </p>
 
-    <p v-if="timeRecord.categoryName" class="text-lg font-medium">
-      {{ _$t("category") }}:
-      <span class="font-normal">{{ timeRecord.categoryName }}</span>
-    </p>
+      <p v-if="timeRecord.categoryName" class="text-lg font-medium">
+        {{ _$t("category") }}:
+        <span class="font-normal">{{ timeRecord.categoryName }}</span>
+      </p>
 
-    <p v-if="timeRecord.externalLink" class="text-lg font-medium flex gap-2">
-      {{ _$t("externalLink") }}:
+      <p v-if="timeRecord.externalLink" class="text-lg font-medium flex gap-2">
+        {{ _$t("externalLink") }}:
 
-      <span
-        class="max-w-44 overflow-hidden inline-block overflow-ellipsis whitespace-nowrap"
-      >
-        <a
-          :href="timeRecord.externalLink"
-          target="_blank"
-          class="underline hover:text-primary font-normal"
+        <span
+          class="max-w-44 overflow-hidden inline-block overflow-ellipsis whitespace-nowrap"
         >
-          {{ timeRecord.externalLink }}
-        </a>
-      </span>
-    </p>
-  </UCard>
+          <a
+            :href="timeRecord.externalLink"
+            target="_blank"
+            class="underline hover:text-primary font-normal"
+          >
+            {{ timeRecord.externalLink }}
+          </a>
+        </span>
+      </p>
+    </CardContent>
+  </Card>
 </template>
