bc29ae720ba2ba76d712045f41fb07602666eef0
Carlos Júnior <crpu4a@gmail.com>
2025-02-12 09:28:31 -0300

    feat: adaptação de responsividade

--- pages/category.vue
+++ pages/category.vue
@@ -10,7 +10,7 @@ useHead({ title: "Categorias" });
 <template>
   <NuxtLayout name="panel-v2">
     <div class="grid grid-cols-1 md:grid-cols-12 gap-16 md:gap-5 w-full">
-      <div class="w-full col-span-1 md:col-span-6 lg:col-span-4">
+      <div class="w-full col-span-1 md:col-span-12 lg:col-span-6">
         <CategoryTable />
       </div>
     </div>
