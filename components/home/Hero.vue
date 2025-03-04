9e241e892700dfd2ec7a038c348dcf7d3d70bab0
Carlos Júnior <crpu4a@gmail.com>
2025-03-04 13:54:48 -0300

    feat: adição de componentes de menu e home

---
+++ components/home/Hero.vue
@@ -0,0 +1,120 @@
+<script setup lang="ts">
+import { ArrowRight } from "lucide-vue-next";
+</script>
+
+<template>
+  <section class="container">
+    <div
+      class="grid place-items-center lg:max-w-screen-xl gap-8 mx-auto py-20 md:py-32"
+    >
+      <div class="text-center space-y-8">
+        <!-- <Badge variant="outline" class="text-sm py-2">
+          <span class="mr-2">
+            <Badge>New</Badge>
+          </span>
+          <span> Design is out now! </span>
+        </Badge> -->
+
+        <div
+          class="max-w-screen-md mx-auto text-center text-5xl md:text-5xl font-bold"
+        >
+          <h1>
+            Gerencie seu tempo de uma forma
+            <span
+              class="text-transparent bg-gradient-to-r from-[#D247BF] to-primary bg-clip-text"
+            >
+              mais inteligente!
+            </span>
+          </h1>
+        </div>
+
+        <p class="max-w-screen-sm mx-auto text-xl text-muted-foreground">
+          Utilize nossas ferramentas para compreender melhor seu tempo gasto
+          estudando, trabalhando ou em seus projetos pessoais.
+        </p>
+
+        <div class="space-y-4 md:space-y-0 md:space-x-4">
+          <Button
+            class="w-5/6 md:w-1/4 font-bold group/arrow"
+            @click="$router.push({ name: 'register' })"
+          >
+            Criar Conta
+
+            <ArrowRight
+              class="size-5 ml-2 group-hover/arrow:translate-x-1 transition-transform"
+            />
+          </Button>
+
+          <Button
+            variant="secondary"
+            class="w-5/6 md:w-1/4 font-bold"
+            @click="$router.push({ name: 'timer.page' })"
+          >
+            Acessar Timer
+          </Button>
+        </div>
+      </div>
+
+      <div class="relative group mt-40 py-12">
+        <!-- gradient shadow -->
+        <div
+          class="absolute -top-6 right-12 w-[90%] h-12 lg:h-[80%] bg-primary/50 blur-3xl rounded-full img-shadow-animation"
+        ></div>
+
+        <!-- <img
+          class="w-full md:w-[1200px] mx-auto rounded-lg relative rouded-lg leading-none flex items-center border border-t-2 border-t-primary/30 img-border-animation"
+          :src="
+            mode == 'light' ? 'hero-image-light.jpg' : 'hero-image-dark.jpg'
+          "
+          alt="dashboard using shadcn-vue"
+        /> -->
+
+        <slot></slot>
+
+        <!-- gradient effect img -->
+        <div
+          class="absolute bottom-0 left-0 w-full h-20 md:h-28 bg-gradient-to-b from-background/0 via-background/50 to-background rounded-lg"
+        ></div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<style scoped>
+.img-shadow-animation {
+  animation-name: img-shadow-animation;
+  animation-iteration-count: infinite;
+  animation-duration: 2s;
+  animation-timing-function: linear;
+  animation-direction: alternate;
+}
+
+.img-border-animation {
+  animation-name: img-border-animation;
+  animation-iteration-count: infinite;
+  animation-duration: 2s;
+  animation-timing-function: linear;
+  animation-direction: alternate;
+}
+
+@keyframes img-shadow-animation {
+  from {
+    opacity: 0.1;
+    transform: translateY(30px);
+  }
+
+  to {
+    opacity: 0.5;
+    transform: translateY(0px);
+  }
+}
+@keyframes img-border-animation {
+  from {
+    @apply border-t-primary/10;
+  }
+
+  to {
+    @apply border-t-primary/60;
+  }
+}
+</style>
---
+++ components/home/Navbar.vue
@@ -0,0 +1,214 @@
+<script lang="ts" setup>
+import { ref } from "vue";
+
+const authStore = useAuthStore();
+const { isAuth: userIsAuth } = storeToRefs(authStore);
+
+import { useColorMode } from "@vueuse/core";
+const mode = useColorMode();
+mode.value = "dark";
+
+import { Menu } from "lucide-vue-next";
+
+interface RouteProps {
+  name: string;
+  label: string;
+}
+
+interface FeatureProps {
+  title: string;
+  description: string;
+}
+
+const routeList = computed<RouteProps[]>(() => {
+  const routes = [
+    {
+      name: "timer.page",
+      label: "Cronômetros",
+    },
+  ];
+
+  if (!userIsAuth.value) {
+    routes.push(
+      {
+        name: "login",
+        label: "Acessar",
+      },
+      {
+        name: "register",
+        label: "Registrar",
+      }
+    );
+  } else {
+    routes.unshift({
+      name: "record.panel",
+      label: "Painel",
+    });
+    routes.push({
+      name: "logout",
+      label: "Sair",
+    });
+  }
+
+  return routes;
+});
+
+// const featureList: FeatureProps[] = [
+//   {
+//     title: "Showcase Your Value ",
+//     description: "Highlight how your product solves user problems.",
+//   },
+//   {
+//     title: "Build Trust",
+//     description:
+//       "Leverages social proof elements to establish trust and credibility.",
+//   },
+//   {
+//     title: "Capture Leads",
+//     description:
+//       "Make your lead capture form visually appealing and strategically.",
+//   },
+// ];
+
+const isOpen = ref<boolean>(false);
+</script>
+
+<template>
+  <header
+    :class="{
+      'shadow-light': mode === 'light',
+      'shadow-dark': mode === 'dark',
+      'w-[90%] md:w-[70%] lg:w-[75%] lg:max-w-screen-xl top-5 mx-auto sticky border z-40 rounded-2xl flex justify-between items-center p-2 bg-card shadow-md': true,
+    }"
+  >
+    <NuxtLink
+      :to="{ name: 'home' }"
+      class="font-bold text-lg flex items-center ml-2"
+    >
+      <h1
+        class="text-transparent bg-gradient-to-r from-blue-500 to-primary bg-clip-text"
+      >
+        Registra meu tempo aí!
+      </h1>
+    </NuxtLink>
+    <!-- Mobile -->
+    <div class="flex items-center lg:hidden">
+      <Sheet v-model:open="isOpen">
+        <SheetTrigger as-child>
+          <Menu @click="isOpen = true" class="cursor-pointer" />
+        </SheetTrigger>
+
+        <SheetContent
+          side="left"
+          class="flex flex-col justify-between rounded-tr-2xl rounded-br-2xl bg-card"
+        >
+          <div>
+            <SheetHeader class="mb-4 ml-4">
+              <SheetTitle class="flex items-center">
+                <NuxtLink
+                  :to="{ name: 'home' }"
+                  @click="isOpen = false"
+                  class="font-bold text-lg flex items-center ml-2"
+                >
+                  <h1
+                    class="text-transparent bg-gradient-to-r from-blue-500 to-primary bg-clip-text"
+                  >
+                    Rmta!
+                  </h1>
+                </NuxtLink>
+              </SheetTitle>
+            </SheetHeader>
+
+            <div class="flex flex-col gap-2">
+              <Button
+                v-for="{ name, label } in routeList"
+                :key="label"
+                as-child
+                variant="ghost"
+                class="justify-start text-base"
+              >
+                <NuxtLink
+                  @click="isOpen = false"
+                  :to="{ name }"
+                  active-class="text-primary font-bold"
+                >
+                  {{ label }}
+                </NuxtLink>
+              </Button>
+            </div>
+          </div>
+
+          <SheetFooter class="flex-col sm:flex-col justify-start items-start">
+            <Separator class="mb-2" />
+
+            <HomeToggleTheme />
+          </SheetFooter>
+        </SheetContent>
+      </Sheet>
+    </div>
+
+    <!-- Desktop -->
+    <NavigationMenu class="hidden lg:block">
+      <NavigationMenuList>
+        <!-- <NavigationMenuItem>
+          <NavigationMenuTrigger class="bg-card text-base">
+            Features
+          </NavigationMenuTrigger>
+          <NavigationMenuContent>
+            <div class="grid w-[600px] grid-cols-2 gap-5 p-4">
+              <img
+                src="https://www.radix-vue.com/logo.svg"
+                alt="Beach"
+                class="h-full w-full rounded-md object-cover"
+              />
+              <ul class="flex flex-col gap-2">
+                <li
+                  v-for="{ title, description } in featureList"
+                  :key="title"
+                  class="rounded-md p-3 text-sm hover:bg-muted"
+                >
+                  <p class="mb-1 font-semibold leading-none text-foreground">
+                    {{ title }}
+                  </p>
+                  <p class="line-clamp-2 text-muted-foreground">
+                    {{ description }}
+                  </p>
+                </li>
+              </ul>
+            </div>
+          </NavigationMenuContent>
+        </NavigationMenuItem> -->
+
+        <NavigationMenuItem>
+          <NavigationMenuLink asChild>
+            <Button
+              v-for="{ name, label } in routeList"
+              :key="label"
+              as-child
+              variant="ghost"
+              class="justify-start text-base"
+            >
+              <NuxtLink :to="{ name }" active-class="text-primary font-bold">
+                {{ label }}
+              </NuxtLink>
+            </Button>
+          </NavigationMenuLink>
+        </NavigationMenuItem>
+      </NavigationMenuList>
+    </NavigationMenu>
+
+    <div class="hidden lg:flex">
+      <HomeToggleTheme />
+    </div>
+  </header>
+</template>
+
+<style scoped>
+.shadow-light {
+  box-shadow: inset 0 0 5px rgba(0, 0, 0, 0.085);
+}
+
+.shadow-dark {
+  box-shadow: inset 0 0 5px rgba(255, 255, 255, 0.141);
+}
+</style>
