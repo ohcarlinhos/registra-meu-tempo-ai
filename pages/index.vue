9d01886a0096f6c85d0749b13aedbf2da18c8f71
Carlos Júnior <crpu4a@gmail.com>
2025-03-04 13:55:19 -0300

    feat: atualização da página home

--- pages/index.vue
+++ pages/index.vue
@@ -5,58 +5,45 @@ useHead({ title: "Cronômetros" });
 
 <template>
   <NuxtLayout name="home">
-    <section class="flex flex-col max-md:gap-8 gap-16">
-      <section class="flex flex-col gap-8 items-center">
-        <h2 class="text-7xl font-bold text-center">Gerencie seu tempo!</h2>
+    <HomeHero>
+      <section class="flex flex-col max-md:gap-8 gap-16 pb-20">
+        <section class="max-md:flex-col flex gap-16">
+          <ClientOnly>
+            <TimerDefault title="Cronômetros" />
 
-        <p class="text-xl text-center">
-          Utilize nossas ferramentas para
-          <b class="text-primary">compreender melhor seu tempo gasto</b>
-          <br class="max-md:hidden" />
-          estudando, trabalhando ou em seus projetos pessoais.
-        </p>
-      </section>
+            <template #fallback>
+              <div class="flex gap-5 flex-col w-[320px] items-center">
+                <Skeleton class="h-[320px] w-[320px] rounded-full" />
+              </div>
+            </template>
+          </ClientOnly>
 
-      <section class="max-md:flex-col flex gap-8">
-        <ClientOnly>
-          <TimerDefault title="Cronômetros" />
+          <section
+            class="flex flex-col gap-5 max-md:items-center items-start z-50"
+          >
+            <h3 class="text-4xl font-bold leading-none max-md:text-center">
+              Que tal começar <br class="max-md:hidden" />
+              por nosso cronômetro?
+            </h3>
 
-          <template #fallback>
-            <div class="flex gap-5 flex-col w-[320px] items-center">
-              <Skeleton class="h-[320px] w-[320px] rounded-full" />
-            </div>
-          </template>
-        </ClientOnly>
+            <p class="text-xl max-md:text-center">
+              É extremante fácil, <b>clique no botão começar (play)</b>
+              <br class="max-md:hidden" />
+              e comece a utilizar diretamente do navegador.
+            </p>
 
-        <section class="flex flex-col gap-5 max-md:items-center items-start">
-          <h3 class="text-4xl font-bold leading-none max-md:text-center">
-            Que tal começar <br class="max-md:hidden" />
-            por nossos cronômetros?
-          </h3>
-
-          <p class="text-xl max-md:text-center">
-            É extremante fácil, <b>clique no botão começar (play)</b>
-            <br class="max-md:hidden" />
-            e comece a utilizar diretamente do navegador.
-          </p>
-
-          <p class="text-lg max-md:text-center">
-            Prefere uma tela sem muita informação? <br />
-            <NuxtLink
-              :to="{ name: 'timer.page' }"
-              class="text-primary font-bold hover:underline"
-            >
-              Acessar página do cronômetro.
-            </NuxtLink>
-          </p>
+            <p class="text-lg max-md:text-center">
+              Prefere uma tela sem muita informação? <br />
+              <NuxtLink
+                :to="{ name: 'timer.page' }"
+                class="text-primary font-bold hover:underline"
+              >
+                Acessar página do cronômetro.
+              </NuxtLink>
+            </p>
+          </section>
         </section>
       </section>
-
-      <!-- <section class="flex flex-col">
-      Tenha maior controle do seu tempo, independente da forma que deseja
-      registrá-lo. Utilizando o cronômetro do seu celular, seu relógio,
-      ampulheta ou nossos timer digitais.
-    </section> -->
-    </section>
+    </HomeHero>
   </NuxtLayout>
 </template>
