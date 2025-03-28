31b4ed40fce54fa0a4e2ed9fa02515ef939bc76c
Carlos Roberto <crpu4a@gmail.com>
2025-03-28 14:04:05 -0300

    fix: limpando layout panel

--- layouts/panel.vue
+++ layouts/panel.vue
@@ -19,24 +19,6 @@ const data = {
       type: "Ferramenta",
     },
   ],
-  // documentation: [
-  //   {
-  //     title: "",
-  //     url: "#",
-  //     icon: BookOpenText,
-  //     isActive: false,
-  //     items: [
-  //       {
-  //         title: "Tarefas",
-  //         name: "record.panel",
-  //       },
-  //       {
-  //         title: "Categorias",
-  //         name: "record.category",
-  //       },
-  //     ],
-  //   },
-  // ],
 
   pages: [
     {
@@ -125,9 +107,6 @@ function setActiveProduct(team: (typeof data.products)[number]) {
                       <component :is="product.logo" class="size-4 shrink-0" />
                     </div>
                     {{ product.name }}
-                    <!-- <DropdownMenuShortcut>
-                      ⌘{{ index + 1 }}
-                    </DropdownMenuShortcut> -->
                   </DropdownMenuItem>
 
                   <DropdownMenuSeparator />
@@ -164,100 +143,13 @@ function setActiveProduct(team: (typeof data.products)[number]) {
                     <span>{{ item.title }}</span>
                   </NuxtLink>
                 </SidebarMenuButton>
-
-                <!-- <DropdownMenu>
-                  <DropdownMenuTrigger as-child>
-                    <SidebarMenuAction show-on-hover>
-                      <MoreHorizontal />
-                      <span class="sr-only">More</span>
-                    </SidebarMenuAction>
-                  </DropdownMenuTrigger>
-                  <DropdownMenuContent
-                    class="w-48 rounded-lg"
-                    side="bottom"
-                    align="end"
-                  >
-                    <DropdownMenuItem>
-                      <Folder class="text-muted-foreground" />
-                      <span>View Project</span>
-                    </DropdownMenuItem>
-                    <DropdownMenuItem>
-                      <Forward class="text-muted-foreground" />
-                      <span>Share Project</span>
-                    </DropdownMenuItem>
-                    <DropdownMenuSeparator />
-                    <DropdownMenuItem>
-                      <Trash2 class="text-muted-foreground" />
-                      <span>Delete Project</span>
-                    </DropdownMenuItem>
-                  </DropdownMenuContent>
-                </DropdownMenu> -->
               </SidebarMenuItem>
-
-              <!-- <SidebarMenuItem>
-              <SidebarMenuButton class="text-sidebar-foreground/70">
-                <MoreHorizontal class="text-sidebar-foreground/70" />
-                <span>More</span>
-              </SidebarMenuButton>
-            </SidebarMenuItem> -->
-
-              <!-- <SidebarGroup>
-                <SidebarGroupLabel>Ajuda</SidebarGroupLabel>
-                <SidebarMenu>
-                  <Collapsible
-                    v-for="item in data.documentation"
-                    :key="item.title"
-                    as-child
-                    :default-open="item.isActive"
-                    class="group/collapsible"
-                  >
-                    <SidebarMenuItem>
-                      <CollapsibleTrigger as-child>
-                        <SidebarMenuButton :tooltip="item.title">
-                          <component :is="item.icon" />
-                          <span>{{ item.title }}</span>
-                          <ChevronRight
-                            class="ml-auto transition-transform duration-200 group-data-[state=open]/collapsible:rotate-90"
-                          />
-                        </SidebarMenuButton>
-                      </CollapsibleTrigger>
-
-                      <CollapsibleContent>
-                        <SidebarMenuSub>
-                          <SidebarMenuSubItem
-                            v-for="subItem in item.items"
-                            :key="subItem.title"
-                          >
-                            <SidebarMenuSubButton as-child>
-                              <NuxtLink
-                                :to="{ name: subItem.name }"
-                                class="hover:text-primary hover:underline text-sm"
-                                active-class="text-primary"
-                              >
-                                <span>{{ subItem.title }}</span>
-                              </NuxtLink>
-                            </SidebarMenuSubButton>
-                          </SidebarMenuSubItem>
-                        </SidebarMenuSub>
-                      </CollapsibleContent>
-                    </SidebarMenuItem>
-                  </Collapsible>
-                </SidebarMenu>
-              </SidebarGroup> -->
-
-              <!-- <SidebarMenuItem>
-                <SidebarMenuButton as-child>
-                  <NuxtLink :to="{ name: 'feedback.page' }" as="a">
-                    <component :is="MessageCircle" />
-                    <span>Enviar Feedback</span>
-                  </NuxtLink>
-                </SidebarMenuButton>
-              </SidebarMenuItem> -->
             </SidebarMenu>
           </SidebarGroup>
         </SidebarContent>
 
         <LayoutPanelFooter />
+
         <SidebarRail />
       </Sidebar>
 
@@ -272,12 +164,6 @@ function setActiveProduct(team: (typeof data.products)[number]) {
 
             <Breadcrumb>
               <BreadcrumbList>
-                <!-- <BreadcrumbItem class="hidden md:block">
-                  <BreadcrumbLink href="/">
-                    Registra meu tempo aí!
-                  </BreadcrumbLink>
-                </BreadcrumbItem> -->
-
                 <template v-for="(item, index) in breadcrumbPage">
                   <BreadcrumbSeparator v-if="index" class="hidden md:block" />
 
@@ -295,16 +181,10 @@ function setActiveProduct(team: (typeof data.products)[number]) {
                 </template>
               </BreadcrumbList>
             </Breadcrumb>
-            <!--  -->
           </div>
         </header>
-        <div class="flex flex-1 flex-col gap-4 p-4 pt-0">
-          <!-- <div class="grid auto-rows-min gap-4 md:grid-cols-3">
-          <div class="aspect-video rounded-xl bg-muted/50" />
-          <div class="aspect-video rounded-xl bg-muted/50" />
-          <div class="aspect-video rounded-xl bg-muted/50" />
-        </div> -->
 
+        <div class="flex flex-1 flex-col gap-4 p-4 pt-0">
           <div class="min-h-[100vh] flex-1 rounded-xl md:min-h-min">
             <slot></slot>
           </div>
