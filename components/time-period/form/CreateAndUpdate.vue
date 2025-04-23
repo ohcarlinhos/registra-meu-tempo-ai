7d2dd6f0e8f4c6b11e91b4ae872a86923d648b18
Carlos Júnior <crpu4a@gmail.com>
2025-04-23 15:33:38 -0300

    feat: migração de componentes de nuxt ui para shadcn e lógica de formulário

--- components/time-period/form/CreateAndUpdate.vue
+++ components/time-period/form/CreateAndUpdate.vue
@@ -1,5 +1,7 @@
 <script lang="ts" setup>
+import { toTypedSchema } from "@vee-validate/yup";
 import { addMinutes, isBefore } from "date-fns";
+import { useForm } from "vee-validate";
 import * as yup from "yup";
 
 const emit = defineEmits(["close", "refresh"]);
@@ -12,40 +14,42 @@ const props = withDefaults(
   {}
 );
 
-const form = reactive<TimePeriodForm>({
-  timeRecordId: 0,
-  id: undefined,
-  start: "",
-  end: "",
-  callback: undefined,
-});
-
-const schema = yup.object({
-  start: yup.string().required(),
-  end: yup.string().required(),
-});
+const formSchema = toTypedSchema(
+  yup.object({
+    start: yup.string().required('O campo "Início" é obrigatório.'),
+    end: yup.string().required('O campo "Fim" é obrigatório.'),
+  })
+);
 
 const closeModal = (refresh = false) => {
   emit("close", refresh);
 };
 
-const submit = async () => {
-  schema.validate(form).then(async () => {
-    await submitAction();
-  });
-};
+const {
+  handleSubmit,
+  values: formValues,
+  setValues,
+} = useForm({
+  validationSchema: formSchema,
+});
 
-const isFetch = ref(false);
+const onSubmit = handleSubmit((value) => {
+  return props.editObject?.id
+    ? editAction(props.editObject.id, value.start, value.end)
+    : createAction(value.start, value.end);
+});
 
-const createAction = async () => {
-  isFetch.value = true;
+const isFetching = ref(false);
+
+const createAction = async (start: string, end: string) => {
+  isFetching.value = true;
   let submitIsOk = true;
 
   try {
     await postTimePeriod({
       timeRecordId: props.timeRecordId!,
-      start: new Date(form.start),
-      end: new Date(form.end),
+      start: new Date(start),
+      end: new Date(end),
     });
 
     OkToast(_$t("createTimePeriodSuccess"));
@@ -54,40 +58,49 @@ const createAction = async () => {
     submitIsOk = false;
     ErrorToast(error);
   } finally {
-    if (submitIsOk && form.callback) form.callback();
-    isFetch.value = false;
+    if (submitIsOk && props.editObject?.callback) {
+      props.editObject.callback();
+    }
+
+    isFetching.value = false;
   }
 };
 
-const editAction = async (id: number) => {
-  isFetch.value = true;
+const editAction = async (id: number, start: string, end: string) => {
+  isFetching.value = true;
+  let submitIsOk = true;
 
   try {
     await putTimePeriod(id, {
-      timeRecordId: form.timeRecordId!,
-      start: new Date(form.start),
-      end: new Date(form.end),
+      timeRecordId: props.timeRecordId!,
+      start: new Date(start),
+      end: new Date(end),
     });
 
-    if (form.callback) form.callback();
+    if (props.editObject?.callback) {
+      props.editObject.callback();
+    }
 
     OkToast(_$t("updateTimePeriodSuccess"));
     closeModal(true);
   } catch (error) {
+    submitIsOk = false;
     ErrorToast(error);
   } finally {
-    isFetch.value = false;
+    if (submitIsOk && props.editObject?.callback) {
+      props.editObject.callback();
+    }
+
+    isFetching.value = false;
   }
 };
 
 const changeStart = (start: string) => {
-  if (!form.end || isBefore(form.end, start)) form.end = addMinutes(start, 10);
-};
-
-const submitAction = async () => {
-  return props.editObject?.id
-    ? editAction(props.editObject.id)
-    : createAction();
+  if (!formValues.end || isBefore(formValues.end, start)) {
+    setValues({
+      end: addMinutes(start, 10).toString(),
+    });
+  }
 };
 
 /**
@@ -98,11 +111,12 @@ watch(
   () => props.editObject,
   (newObj) => {
     if (newObj) {
-      form.id = newObj.id;
-      form.start = newObj.start;
-      form.end = newObj.end;
-      form.timeRecordId = newObj.timeRecordId;
-      form.callback = newObj.callback;
+      const { start, end } = newObj;
+
+      setValues({
+        start: start.toString(),
+        end: end.toString(),
+      });
     }
   },
   {
@@ -114,29 +128,57 @@ var dateMax = ref(addMinutes(Date.now(), 10));
 </script>
 
 <template>
-  <UForm :schema="schema" :state="form" @submit="submit" class="space-y-4">
+  <form @submit="onSubmit" class="space-y-4">
     <div class="flex gap-4 relative">
-      <UFormGroup :label="_$t('startOfPeriod')" name="start">
-        <GDatePicker
-          v-model="form.start"
-          :disabled="isFetch"
-          :max="dateMax"
-          class="py-1"
-          @change="changeStart"
-        />
-      </UFormGroup>
+      <FormField v-slot="{ componentField }" name="start">
+        <FormItem>
+          <FormLabel>{{ _$t("startOfPeriod") }}</FormLabel>
 
-      <UFormGroup :label="_$t('endOfPeriod')" name="end">
-        <GDatePicker
-          v-model="form.end"
-          :min="form.start"
-          :max="dateMax"
-          :disabled="isFetch"
-          class="py-1"
-        />
-      </UFormGroup>
+          <FormControl>
+            <GDatePicker
+              class="py-1"
+              :model-value="componentField.modelValue"
+              @change="
+                setValues({
+                  start: $event,
+                });
+                changeStart($event);
+              "
+              :max="dateMax"
+              :disabled="isFetching"
+            />
+          </FormControl>
+          <FormMessage />
+        </FormItem>
+      </FormField>
+
+      <FormField v-slot="{ componentField }" name="end">
+        <FormItem>
+          <FormLabel>{{ _$t("endOfPeriod") }}</FormLabel>
+
+          <FormControl>
+            <GDatePicker
+              class="py-1"
+              :model-value="componentField.modelValue"
+              @change="
+                setValues({
+                  end: $event,
+                })
+              "
+              :min="formValues.start"
+              :max="dateMax"
+              :disabled="isFetching"
+            />
+          </FormControl>
+          <FormMessage />
+        </FormItem>
+      </FormField>
     </div>
 
-    <UButton :loading="isFetch" :label="$t('send')" block type="submit" />
-  </UForm>
+    <Button :loading="isFetching" class="w-full mt-2" type="submit">
+      <template #with-loading>
+        {{ _$t("send") }}
+      </template>
+    </Button>
+  </form>
 </template>
