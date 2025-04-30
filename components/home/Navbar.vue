52e251c86fc6de564e1649a584c7c343e60fea65
Carlos Júnior <crpu4a@gmail.com>
2025-04-30 10:39:22 -0300

    fix: exclusão de comentários

--- components/home/Navbar.vue
+++ components/home/Navbar.vue
@@ -1,9 +1,8 @@
 <script lang="ts" setup>
 import { ref } from "vue";
+import { Menu } from "lucide-vue-next";
 
 const { loggedIn } = useUserSession();
-
-import { Menu } from "lucide-vue-next";
 
 interface RouteProps {
   name: string;
@@ -41,23 +40,6 @@ const routeList = computed<RouteProps[]>(() => {
 
   return routes;
 });
-
-// const featureList: FeatureProps[] = [
-//   {
-//     title: "Showcase Your Value ",
-//     description: "Highlight how your product solves user problems.",
-//   },
-//   {
-//     title: "Build Trust",
-//     description:
-//       "Leverages social proof elements to establish trust and credibility.",
-//   },
-//   {
-//     title: "Capture Leads",
-//     description:
-//       "Make your lead capture form visually appealing and strategically.",
-//   },
-// ];
 
 const isOpen = ref<boolean>(false);
 </script>
@@ -136,35 +118,6 @@ const isOpen = ref<boolean>(false);
     <!-- Desktop -->
     <NavigationMenu class="hidden lg:block">
       <NavigationMenuList>
-        <!-- <NavigationMenuItem>
-          <NavigationMenuTrigger class="bg-card text-base">
-            Features
-          </NavigationMenuTrigger>
-          <NavigationMenuContent>
-            <div class="grid w-[600px] grid-cols-2 gap-5 p-4">
-              <img
-                src="https://www.radix-vue.com/logo.svg"
-                alt="Beach"
-                class="h-full w-full rounded-md object-cover"
-              />
-              <ul class="flex flex-col gap-2">
-                <li
-                  v-for="{ title, description } in featureList"
-                  :key="title"
-                  class="rounded-md p-3 text-sm hover:bg-muted"
-                >
-                  <p class="mb-1 font-semibold leading-none text-foreground">
-                    {{ title }}
-                  </p>
-                  <p class="line-clamp-2 text-muted-foreground">
-                    {{ description }}
-                  </p>
-                </li>
-              </ul>
-            </div>
-          </NavigationMenuContent>
-        </NavigationMenuItem> -->
-
         <NavigationMenuItem>
           <NavigationMenuLink asChild>
             <Button
