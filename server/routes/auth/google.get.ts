3d835701c3b481954782cb733e65ea9ed2ed23e8
Carlos Júnior <crpu4a@gmail.com>
2025-05-03 17:44:02 -0300

    feat: adição de rota de autenticação com google

---
+++ server/routes/auth/google.get.ts
@@ -0,0 +1,42 @@
+import { JwtData } from "~/utils/types/api/map/JwtData";
+
+export default defineOAuthGoogleEventHandler({
+  async onSuccess(event, { tokens }) {
+    const { externalBaseURL } = useRuntimeConfig();
+
+    try {
+      const result = await $fetch<JwtData>("/auth/login/google", {
+        baseURL: externalBaseURL,
+        method: "POST",
+        body: {
+          accessToken: tokens.access_token,
+          tokenType: tokens.token_type,
+        },
+        headers: {
+          "User-Agent": event.headers.get("User-Agent")?.toString() || "",
+          "X-Forwarded-For":
+            event.node.req.headers["x-forwarded-for"]?.toString() || "",
+        },
+      });
+
+      const claim = decodeJwtToken(result.token);
+
+      await setUserSession(event, {
+        token: result.token,
+        user: {
+          name: claim.name,
+          email: claim.email,
+        },
+      });
+    } catch (err) {
+      console.log(err);
+    }
+
+    return sendRedirect(event, "/");
+  },
+
+  onError(event, error) {
+    console.error("Google OAuth error:", error);
+    return sendRedirect(event, "/");
+  },
+});
