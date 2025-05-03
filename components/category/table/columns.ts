0e0219c0ab740a605c948c0b8188e8c47f47eedb
Carlos Júnior <crpu4a@gmail.com>
2025-05-03 15:01:59 -0300

    refactor: exclusão de importações

--- components/category/table/columns.ts
+++ components/category/table/columns.ts
@@ -1,10 +1,7 @@
-import { Button, NuxtLink } from "#components";
 import type { ColumnDef } from "@tanstack/vue-table";
 import { h } from "vue";
 
 import Actions from "./Actions.vue";
-
-import { AArrowDownIcon, AArrowUpIcon } from "lucide-vue-next";
 
 export const columns: ColumnDef<CategoryMap>[] = [
   {
